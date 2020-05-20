02e1a0602b4cb274b2f2fa110f53e4afbe6a8ae5
d7p4n4 <bercikezs199999999@gmail.com>
2020-05-20 13:12:47 +0200

    1.20200520.1

---
+++ CSGIGRESTResponseRequestClassesCORE/GetUserFromByTokenRequest.cs
@@ -0,0 +1,12 @@
+﻿using Modul.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSGIGRESTResponseRequestClassesCORE
+{
+    public class GetUserFromByTokenReqest : Ac4yServiceRequest
+    {
+        public string fbToken { get; set; }
+    }
+}
---
+++ CSGIGRESTResponseRequestClassesCORE/GetUserFromByTokenResponse.cs
@@ -0,0 +1,14 @@
+﻿using CSGIGUserServer;
+using Modul.Final.Class;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSGIGRESTResponseRequestClassesCORE
+{
+    public class GetUserFromByTokenResponse : Ac4yServiceResponse
+    {
+        public User User { get; set; }
+        public string Json { get; set; }
+    }
+}
